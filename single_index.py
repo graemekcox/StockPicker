9c0909b0fa7495a358ee72831098c28a4f51f469
Graeme <graeme.k.cox@gmail.com>
2020-01-26 16:26:06 -0500

    Move functions to 'Ticker' class, and start adding single-index analysis functions

--- single_index.py
+++ single_index.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from scipy import stats
 from pandas_datareader import data
-
+from ticker import *
+from portfolio import *
 
 
 # stocks = ["SPY"]
@@ -16,79 +17,46 @@ from pandas_datareader import data
 start = dt.datetime(2016,10,6)
 end = dt.datetime(2019,10,4)
 
+
+# ticker_name = ''
+# alpha = 0
+# beta = 0
+# average_returns = 0
+# var = 0
+# err = 0
+# std_dev = 0
+# r_sqrd = 0
+
 # end = dt.datetime.now()
-# stocks = ['AAPL','AMZN','GOOGL','FB', 'MSFT']
-stocks = ['AAPL']
+tickers = ['AAPL','MSFT']
 
-# data = quandl.get_table('WIKI/PRICES', ticker = stocks,
-#                         qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
-#                         date = { 'gte': '2016-1-1', 'lte': '2017-12-31' }, paginate=True)
+# portfolio = Portfolio(tickers, start, end)
 
-# https://www.quandl.com/api/v3/databases/WIKI/metadata?api_key=%3CYOURAPIKEY%3E
-# df = data.set_index('date')
-# table = df.pivot(columns='ticker')
-# # By specifying col[1] in below list comprehension
-# # You can select the stock names under multi-level column
-# table.columns = [col[1] for col in table.columns]
+stocks = {}
+rf_rate = 1.9 #Risk free rate. Set to 1.9%
 
-# returns = table.pct_change() # Daily pct change
+portfolio = {}
+excess_returns = {}
 
-## Can't get SPY from quandl for free
-df = web.DataReader('AAPL', 'yahoo', start, end)
-df_spy = web.DataReader('SPY', 'yahoo', start, end)
+for ticker in tickers:
+    temp = {}
+    stocks[ticker] = Ticker(ticker, 'SPY', start, end)
+    # temp['Stock'] = Ticker(ticker, 'SPY', start, end)
+    temp['Excess Returns'] = stocks[ticker].average_return - (rf_rate/252)
+    temp['Excess Returns over Beta'] = temp['Excess Returns'] / stocks[ticker].beta
+    
+    portfolio[ticker] = temp
 
-df_spy.reset_index(inplace=True)
-# df_spy.drop(['High','Close','Low','Open','Volume'], axis=1,inplace=True)
-df_spy.set_index("Date", inplace=True)
+    print('Processed %s'%ticker)
+    print(portfolio[ticker])
 
-def calculate_CAPM(df_stock, df_market, show_plot=False):
-    df_stock['Returns'] = df_stock['Adj Close'].pct_change() * 100
-    df_market['Returns'] = df_market['Adj Close'].pct_change() * 100
-    df_stock['Market Returns'] = df_market['Returns']
-    df_subset = df_stock.dropna(subset=['Returns', 'Market Returns'])
 
-    if show_plot:
-        df_stock.plot(x='Market Returns', y='Stock Returns', kind='scatter')
-        plt.show()
+print(stocks['AAPL'].plt_scatter())
 
-    stock_returns = df_subset['Returns']
-    market_returns = df_subset['Market Returns']
+col_3 = (portfolio['AAPL']['Excess Returns'] * stocks['AAPL'].beta) / stocks['AAPL'].err #unsystematic risk
+print(col_3)
 
-    coeffs = np.polyfit( market_returns, stock_returns, 1)
+# col_4 = (stocks['AAPL'].beta ** 2) / stocks['AAPL'].err
+# print(col_4)
 
-    print(10*'-', 'Linear Regression Results', 10*'-')
-    # TODO, these values are a factor off of the excel document
-    beta = coeffs[1]
-    alpha = coeffs[0]
-    print('Alpha ', alpha)
-    print('Beta ', beta)
-
-    p = np.poly1d(coeffs)
-    print('Equation is  ', p )
-
- 
-    slope, intercept, r_value, p_value, std_err = stats.linregress(market_returns,  stock_returns)
-    print('Slope = ',slope)
-    print('Intercept = ',intercept)
-    print('R value = ',r_value)
-    print('R squared = ' , r_value**2)
-    print('P value = ',p_value)
-    print('Std err = ',std_err)
-    print(45*'-')
-
-    beta = slope
-    alpha = intercept
-    average_return =  stock_returns.mean()
-    print('Average Return = ', average_return)    
-
-    variance = ( stock_returns- average_return)**2 / len( stock_returns) #Find variance in stock returns
-    var = variance.sum()
-
-    print('Var = ', var )
-    print('Std Dev = ', np.sqrt(var))
-
-    errors = (stock_returns - (alpha + beta *  market_returns)) ** 2
-    total_error = errors.sum() / len(errors)
-    print('Error = ', total_error)
-
-calculate_CAPM(df, df_spy)
+# col_5 = 
