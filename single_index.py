191664c07b3b21c55bc41406d094c87fea71b3eb
Graeme <graeme.k.cox@gmail.com>
2020-01-28 23:22:24 -0500

    Create table to determine cutoff rate

--- single_index.py
+++ single_index.py
@@ -8,8 +8,6 @@ import statsmodels.api as sm
 from scipy import stats
 from pandas_datareader import data
 from ticker import *
-from portfolio import *
-
 
 # stocks = ["SPY"]
 # start = dt.datetime(2016,1,1)
@@ -27,7 +25,7 @@ end = dt.datetime(2019,10,4)
 # std_dev = 0
 # r_sqrd = 0
 
-tickers = ['AAPL','MSFT']
+tickers = ['AAPL','MSFT','AMZN','FB']
 
 stocks = {}
 rf_rate = 1.9 #Risk free rate. Set to 1.9%
@@ -38,44 +36,49 @@ excess_returns = {}
 sum_5 = 0
 sum_6 = 0
 
-
-df = pd.DataFrame(index=tickers,columns=["Excess Returns",'Excess Returns with Beta'])
+portfolio = [] # Used for optimal portfolio
 
 for ticker in tickers:
     temp = {}
     stocks[ticker] = Ticker(ticker, 'SPY', start, end)
-    # stocks[ticker].plt_scatter()
 
     excess_returns = stocks[ticker].average_return - (rf_rate/252)
 
-    # df['Excess Returns'][ticker] = excess_returns
-    # df['Excess Returns over Beta'][ticker] = (excess_returns * stocks[ticker].beta)/stocks[ticker].err
-    beta_risk = stocks[ticker].beta ** 2 / stocks[ticker].err
-    
-    sum_excess_returns += (excess_returns * stocks[ticker].beta)/stocks[ticker].err
-    sum_excess_returns
+    temp['Ticker'] = ticker
+    temp['Excess Returns']= excess_returns
+    temp['Excess Returns over Beta'] = (excess_returns * stocks[ticker].beta)/stocks[ticker].err
 
-    sum_beta_risk+= stocks[ticker].beta ** 2 / stocks[ticker].err
+    portfolio.append(temp)
 
-    portfolio[ticker] = temp
     print('Processed %s'%ticker)
-    # portfolio[ticker]['Excess Returns'] = stocks[ticker].average_return - (rf_rate/252)
-    # portfolio[ticker]['Excess Returns over Beta'] = (stocks[ticker].average_return - (rf_rate/252))/stocks[ticker].beta
 
+print(portfolio)
+df = pd.DataFrame(portfolio)
 
-    # print(portfolio[ticker])
+## We want to sort tickers by excess returns over beta
+df = df.sort_values(by=['Excess Returns over Beta'], ascending=False)
 
-# print(stocks['AAPL'].plt_scatter())
+# risk = df.iloc[0]['Excess Returns over Beta']
 
-# col_3 = (portfolio['AAPL']['Excess Returns'] * stocks['AAPL'].beta) / stocks['AAPL'].err #unsystematic risk
-# print(col_3)
+sum_col_3 = 0
+sum_col_4 = 0
 
-# col_4 = (stocks['AAPL'].beta ** 2) / stocks['AAPL'].err
-# print("COL 4 = ",col_4)
+for index, row in df.iterrows():
+    print(row['Ticker'])
+    ticker = row['Ticker']
 
+    col_3 = excess_returns * stocks[ticker].beta/ stocks[ticker].err
+    beta_risk = stocks[ticker].beta ** 2 / stocks[ticker].err
 
+    sum_col_3 += col_3
+    sum_col_4 += beta_risk
 
+    df.at[index, 'Col 3'] =  col_3
+    df.at[index, 'Col 4'] = beta_risk
+    df.at[index, 'Sum Col 3'] = sum_col_3
+    df.at[index, 'Sum Col 4'] = sum_col_4
+
+  
 print(df.head())
-
 
 # col_5 = 
